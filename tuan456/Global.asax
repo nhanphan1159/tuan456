2f793615ae6521aa4bce42534f732988258782a8
nhanphan1159 <nhanphan1159@gmail.com>
2021-07-06 15:41:10 +0700

    Add project files.

---
+++ tuan456/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="tuan456.MvcApplication" Language="C#" %>
---
+++ tuan456/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(tuan456.Startup))]
+namespace tuan456
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
